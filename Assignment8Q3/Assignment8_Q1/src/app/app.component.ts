445fb69b106b127754e758b8a2fe802096dc8911
Pratikshasg17 <79150496+Pratikshasg17@users.noreply.github.com>
2024-01-04 14:34:31 +0530

    Add files via upload

---
+++ Assignment8Q3/Assignment8_Q1/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Assignment8_Q1';
+  public Message : any;
+}
